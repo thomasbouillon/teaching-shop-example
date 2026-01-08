61b793827f3f5652c737541a1f6b38b6cb1d5c1b
Meriem Elachkar <meriemelachkar@gmail.com>
2026-01-08 16:59:19 +0100

    added category field to the product model

--- backend/core/api/models.py
+++ backend/core/api/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('bavoirs', 'Bavoirs'),
+        ('doudous', 'Doudous'),
+        ('couvertures', 'Couvertures'),
+    ]
+    
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     imageUrl = models.CharField(max_length=200)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='bavoirs')
 
     def __str__(self):
         return self.name
