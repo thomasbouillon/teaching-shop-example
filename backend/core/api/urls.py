a51bc1f754801543010d2f16199208474a67791b
BLACHERE Nicolas <nicolas.blachere@gmail.com>
2026-01-08 16:57:57 +0100

    Task 4: Backend - Run Database Migration

--- backend/core/api/urls.py
+++ backend/core/api/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import viewsets, serializers, routers
 
-from .models import Product
+from .models import Product, Review
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -31,9 +31,20 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
 
 router = routers.DefaultRouter()
 router.register(r'products', ProductViewSet)
+router.register(r'reviews', ReviewViewSet)
 
 
 urlpatterns = [
