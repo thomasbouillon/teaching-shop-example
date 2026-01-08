4eacbabdea47874cb98b0eed51691e340163e09c
BLACHERE Nicolas <nicolas.blachere@gmail.com>
2026-01-08 16:56:43 +0100

    Task 3: Backend - Register in Django Admin

--- backend/core/api/admin.py
+++ backend/core/api/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-
-from .models import Product
+from .models import Product, Review
 
 admin.site.register(Product)
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('author', 'product', 'rating', 'created_at')
+    list_filter = ('rating', 'product')
