ff409a45f9b50b965f026be941d9a40da972ac3f
= <=>
2026-01-08 17:22:34 +0100

    Customer Review Url Added

--- backend/core/api/urls.py
+++ backend/core/api/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import viewsets, serializers, routers
 
-from .models import Product
+from .models import Product, Review
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -32,8 +32,19 @@ class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
 
 
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
 router = routers.DefaultRouter()
 router.register(r'products', ProductViewSet)
+router.register(r'reviews', ReviewViewSet)
 
 
 urlpatterns = [
