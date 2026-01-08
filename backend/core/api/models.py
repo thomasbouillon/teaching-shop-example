4db919d9c7feee4097bd4ee94a5c90a2c7279095
BLACHERE Nicolas <nicolas.blachere@gmail.com>
2026-01-08 16:55:32 +0100

    Task 2: Backend - Create Review Model

--- backend/core/api/models.py
+++ backend/core/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Product(models.Model):
@@ -10,3 +11,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    author = models.CharField(max_length=100)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author} - {self.product.name}"
