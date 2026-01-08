9ff7cd20932cdaeb137dbc0b999ba06e6327c33f
Yoan <yoan.nougueruiz@gmail.com>
2026-01-08 16:54:02 +0100

    Register in admin

--- backend/core/api/admin.py
+++ backend/core/api/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, SiteSettings
 
 admin.site.register(Product)
+
+@admin.register(SiteSettings)
+class SiteSettingsAdmin(admin.ModelAdmin):
+    list_display = ('orders_enabled',)
